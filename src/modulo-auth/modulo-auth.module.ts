04bd96ac0b92fd450cb01db78f9505bcf6cd7b89
Dark <djcabrera@sudamericano.edu.ec>
2025-11-27 21:53:05 -0500

    Commit #8
    2. Configurar JWT en el proyecto, utilizando JwtModule y variables de entorno para la clave secreta.
    
    Se toma en cuenta también el tiempo de expiración.

--- src/modulo-auth/modulo-auth.module.ts
+++ src/modulo-auth/modulo-auth.module.ts
@@ -1,4 +1,20 @@
+// src/modulo-auth/modulo-auth.module.ts
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ServicioAuthService } from '../servicio-auth/servicio-auth.service';
 
-@Module({})
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true, // hace accesible JwtService sin volver a importarlo
+      secret: process.env.JWT_SECRETO, // leído desde .env
+      signOptions: {
+        // usamos ?? para poner un valor por defecto y as any para evitar el error de tipos
+        expiresIn: (process.env.JWT_EXPIRA ?? '1h') as any,
+      },
+    }),
+  ],
+  providers: [ServicioAuthService],
+  exports: [ServicioAuthService],
+})
 export class ModuloAuthModule {}
--- src/servicio-auth/servicio-auth.service.ts
+++ src/servicio-auth/servicio-auth.service.ts
@@ -1,4 +1,27 @@
+// src/modulo-auth/servicio-auth.service.ts
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class ServicioAuthService {}
+export class ServicioAuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async login(usuario: { id: number; correo: string }) {
+    const payload = {
+      sub: usuario.id,
+      correo: usuario.correo,
+    };
+
+    const token = await this.jwtService.signAsync(payload);
+
+    return {
+      mensaje: 'Login exitoso',
+      access_token: token,
+    };
+  }
+
+  async validarUsuario(correo: string, contrasenia: string) {
+    // Aquí luego agregarás comparación con bcrypt y Prisma.
+    return null;
+  }
+}
